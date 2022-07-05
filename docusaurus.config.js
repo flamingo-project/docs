9fd26221155083c45469598a6a0f68116a503e2e
Marci <mihmar17@gmail.com>
2022-07-05 19:52:00 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Flamingo Project',
   tagline: '',
   url: 'google.com',
-  baseUrl: '/',
+  baseUrl: '/docs-build/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.jpg',
