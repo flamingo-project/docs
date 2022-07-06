c861a07b498150add6db05729ecdf4a8d4597c21
Marci <mihmar17@gmail.com>
2022-07-06 21:44:52 +0200

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Flamingo Project',
   tagline: '',
   url: 'google.com',
-  baseUrl: '/',
+  baseUrl: '/docs-build/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.jpg',
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'flamingo-project', // Usually your GitHub org/user name.
-  projectName: 'docs-build', // Usually your repo name.
+  projectName: 'docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
